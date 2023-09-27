8798360efc227f09e744c86304cebf776ab43f83
Dickson Wu <wbs.dickson@gmail.com>
2023-09-27 13:43:18 -0600

    add course table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -4,10 +4,10 @@ import { PlusCircle } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
-// import { db } from "@/lib/db";
+import { db } from "@/lib/db";
 
-// import { DataTable } from "./_components/data-table";
-// import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
 
 const CoursesPage = async () => {
     const { userId } = auth();
@@ -16,24 +16,18 @@ const CoursesPage = async () => {
         return redirect("/");
     }
 
-    // const courses = await db.course.findMany({
-    //   where: {
-    //     userId,
-    //   },
-    //   orderBy: {
-    //     createdAt: "desc",
-    //   },
-    // });
+    const courses = await db.course.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
 
     return (
         <div className="p-6">
-            {/* <DataTable columns={columns} data={courses} /> */}
-            <Link href="/teacher/create">
-                <Button>
-                    <PlusCircle className="h-4 w-4 mr-2" />
-                    New course
-                </Button>
-            </Link>
+            <DataTable columns={columns} data={courses} />
         </div>
     );
 };
