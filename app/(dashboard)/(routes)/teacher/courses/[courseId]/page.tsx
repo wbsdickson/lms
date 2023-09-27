e238394b31f2cbe44ad90bb55ef7ebdd2cfc352d
Dickson Wu <wbs.dickson@gmail.com>
2023-09-26 19:35:41 -0600

    complete course form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,16 +4,16 @@ import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-reac
 
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
-// import { Banner } from "@/components/banner";
+import { Banner } from "@/components/banner";
 
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
-// import { PriceForm } from "./_components/price-form";
-// import { AttachmentForm } from "./_components/attachment-form";
-// import { ChaptersForm } from "./_components/chapters-form";
-// import { Actions } from "./_components/actions";
+import { ChaptersForm } from "./_components/chapters-form";
+import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
+import { Actions } from "./_components/actions";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     const { userId } = auth();
@@ -61,7 +61,11 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     ];
 
     const totalFields = requiredFields.length;
-    const completedFields = requiredFields.filter(Boolean).length;
+    console.log(requiredFields);
+    const completedFields = requiredFields.filter((f) => {
+        console.log(`check ${f} = ${Boolean(f)}`);
+        return Boolean(f);
+    }).length;
 
     const completionText = `(${completedFields}/${totalFields})`;
 
@@ -69,23 +73,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 
     return (
         <>
-            course {params.courseId}
-            {/* {!course.isPublished && (
-        <Banner
-          label="This course is unpublished. It will not be visible to the students."
-        />
-      )} */}
+            {!course.isPublished && <Banner label="This course is unpublished. It will not be visible to the students." />}
             <div className="p-6">
                 <div className="flex items-center justify-between">
                     <div className="flex flex-col gap-y-2">
                         <h1 className="text-2xl font-medium">Course setup</h1>
                         <span className="text-sm text-slate-700">Complete all fields {completionText}</span>
                     </div>
-                    {/* <Actions
-            disabled={!isComplete}
-            courseId={params.courseId}
-            isPublished={course.isPublished}
-          /> */}
+                    <Actions disabled={!isComplete} courseId={params.courseId} isPublished={course.isPublished} />
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
                     <div>
@@ -111,30 +106,21 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
                                 <IconBadge icon={ListChecks} />
                                 <h2 className="text-xl">Course chapters</h2>
                             </div>
-                            {/* <ChaptersForm
-                initialData={course}
-                courseId={course.id}
-              /> */}
+                            <ChaptersForm initialData={course} courseId={course.id} />
                         </div>
                         <div>
                             <div className="flex items-center gap-x-2">
                                 <IconBadge icon={CircleDollarSign} />
                                 <h2 className="text-xl">Sell your course</h2>
                             </div>
-                            {/* <PriceForm
-                initialData={course}
-                courseId={course.id}
-              /> */}
+                            <PriceForm initialData={course} courseId={course.id} />
                         </div>
                         <div>
                             <div className="flex items-center gap-x-2">
                                 <IconBadge icon={File} />
                                 <h2 className="text-xl">Resources & Attachments</h2>
                             </div>
-                            {/* <AttachmentForm
-                initialData={course}
-                courseId={course.id}
-              /> */}
+                            <AttachmentForm initialData={course} courseId={course.id} />
                         </div>
                     </div>
                 </div>
